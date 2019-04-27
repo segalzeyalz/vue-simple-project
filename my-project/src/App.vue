63d09f432d9af0ed145c384ced530b63c3a2af72
segalzeyalz <segaleseo@gmail.com>
2019-04-27 14:12:10 +0300

    some structure

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a href="#" class="navbar-brand">E-commerce Inc.</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Cart</a></li>
+                    </ul>
+                    <div class="nav navbar-nav navbar-right">
+                        <div class="stats">{{ cart.items.length }} <template v-if="cart.items.length == 1">item</template><template v-else>items</template> in cart, totalling {{ cartTotal | currency }}</div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <router-view :cart="cart"></router-view>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+    import { eventBus } from './main';
+    import CartMixin from './mixins/cart.js';
+    export default {
+        mixins: [ CartMixin ],
+        data() {
+            return {
+                cart: {
+                    items: []
+                }
+            };
+        },
+        created() {
+            eventBus.$on('addItemToCart', (data) => {
+                this.addProductToCart(data.product, data.quantity);
+            });
+        }
+    }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', He6ארlvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+    .flex { display:flex }
+    .flex.flex-column { flex-direction: column; }
+    .flex.flex-row { flex-direction: row; }
+    .flex.justify-center { justify-content: center; }
+    .flex.justify-left { justify-content: flex-start; }
+    .flex.justify-right { justify-content: flex-end; }
+    .flex.align-center { align-items: center; }
+    .flex.align-left { align-items: flex-start; }
+    .flex.align-right { align-items: flex-end; }
+    /* Navigation */
+    .navbar .stats {
+        margin-top: 15px;
+    }
 </style>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,10 +1,37 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
-import store from './store';
+import ProductList from './components/ProductList.vue';
+import Cart from './components/Cart.vue';
 
-Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [{
+      path: '',
+      component: ProductList
+    },
+    {
+      path: '/cart',
+      component: Cart
+    }
+  ],
+  mode: 'history',
+});
+
+Vue.filter('currency', (value) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+  });
+  return formatter.format(value);
+});
+
+export const eventBus = new Vue();
 
 new Vue({
-  store,
+  el: '#app',
   render: h => h(App),
-}).$mount('#app');
+  router: router,
+});
